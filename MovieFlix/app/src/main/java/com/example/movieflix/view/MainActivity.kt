e6b1db29a144cee0753ee3514634f482a5ae36e8
joseRobertoMoura <jose.roberto.moura@everis.com>
2021-03-30 15:35:45 -0300

    implementação mainactivity e adapter

--- MovieFlix/app/src/main/java/com/example/movieflix/view/MainActivity.kt
+++ MovieFlix/app/src/main/java/com/example/movieflix/view/MainActivity.kt
@@ -1,12 +1,67 @@
 package com.example.movieflix.view
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movieflix.R
+import com.example.movieflix.api.MovieFlixApiTask
+import com.example.movieflix.model.Movie
+import com.example.movieflix.model.MovieTendency
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.moviesList)
+
+        setList()
     }
+
+    private fun setList() {
+        loadingVisibility(true)
+        Thread {
+            val call = MovieFlixApiTask.retrofitApi()
+                .getListTndency("579dbbdd2de6dd3cc42c4d65dc3afdae")
+            call.enqueue(object : Callback<MovieTendency> {
+                override fun onResponse(
+                    call: Call<MovieTendency>,
+                    response: Response<MovieTendency>
+                ) {
+                    if (response.isSuccessful) {
+                        loadingVisibility(false)
+                        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                        val list: MutableList<Movie> = mutableListOf()
+                        response.body()?.result?.forEach { list.add(it) }
+                        recyclerView.adapter = MoviesAdapter(list)
+                    }
+                }
+
+                override fun onFailure(call: Call<MovieTendency>, t: Throwable) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Tivemos um problema, tente novamente em alguns instantes.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
+        }.start()
+    }
+
+    private fun loadingVisibility(isLoading: Boolean) {
+        progress_bar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
 }
+
---
+++ MovieFlix/app/src/main/java/com/example/movieflix/view/MoviesAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.movieflix.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.movieflix.R
+import com.example.movieflix.model.Movie
+
+class MoviesAdapter(private var list: MutableList<Movie>) :
+    RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.movie_item, parent, false)
+        return MoviesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class MoviesViewHolder(
+        itemView: View
+    ) : RecyclerView.ViewHolder(itemView) {
+        private val tvTitl: AppCompatTextView = itemView.findViewById(R.id.movieTitle)
+        private val image: AppCompatImageView = itemView.findViewById(R.id.movieImage)
+
+
+        fun bind(movie: Movie) {
+            if (movie.original_title != null) {
+                tvTitl.text = movie.original_title
+            } else if (movie.original_name != null) {
+                tvTitl.text = movie.original_name
+            }
+            image.load("https://image.tmdb.org/t/p/w500" + movie.poster_path) {
+                placeholder(R.drawable.ic_baseline_image_24)
+                fallback(R.drawable.ic_baseline_image_24)
+            }
+
+        }
+    }
+}
+
+
